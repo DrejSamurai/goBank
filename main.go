46481d53324d25665ed7eaf4f084fd0536193dbc
Andrej Mishevski <andrej.mishevski@gmail.com>
2025-04-16 22:18:23 +0200

    creating tables and handeling creating account

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 )
 
@@ -11,7 +10,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", store)
-	//server := NewAPIServer(":3000", store)
-	//server.Run()
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewAPIServer(":3000", store)
+	server.Run()
 }
